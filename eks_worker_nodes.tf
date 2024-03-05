583704f9a7cdebf3c1e45363b315ff33f1937f01
SAHICHOWDARY <78893457+Sahichowdary@users.noreply.github.com>
2024-03-05 16:33:15 +0530

    Update eks_worker_nodes.tf

--- eks_worker_nodes.tf
+++ eks_worker_nodes.tf
@@ -1,6 +1,6 @@
 resource "aws_eks_node_group" "eks_main" {
   cluster_name = aws_eks_cluster.eks_main.name
-  node_role_arn = aws_iam_role.NodeGroupRole2.arn
+  node_role_arn = var.eks_node_role
   subnet_ids = [aws_subnet.vpc_private_subnet_private_1.id, aws_subnet.vpc_private_subnet_private_2.id]
   scaling_config {
     min_size = 3
